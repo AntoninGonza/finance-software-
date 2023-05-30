a2b604861cd43f4b913905e9eb9438b6a4166389
AntoninGonza <antonin.gonzalez.crohare@gmail.com>
2023-05-30 10:52:11 +0200

    NPV + IRR

--- index.js
+++ index.js
@@ -20,6 +20,65 @@ app.post('/wacc', (req, res) => {
   res.send(`The WACC is ${wacc}%`);
 });
 
+// Handle form submission and calculate NPV
+app.post('/NPV', (req, res) => {
+  const { costOfProject, RevenueYear1, RevenueYear2, RevenueYear3, RevenueYear4, discountRate } = req.body;
+  const r = discountRate / 100; // Convert discount rate to a decimal
+  const NPV = -costOfProject + (RevenueYear1 / (1 + r)) + (RevenueYear2 / Math.pow(1 + r, 2)) + (RevenueYear3 / Math.pow(1 + r, 3)) + (RevenueYear4 / Math.pow(1 + r, 4));
+
+  // Send the NPV result to a new page
+  res.send(`The NPV is ${NPV} $`);
+});
+
+// Handle form submission and calculate IRR
+app.post('/IRR', (req, res) => {
+  const { cashFlows } = req.body;
+  const cashFlowArray = cashFlows.split(',').map(Number);
+
+  // Calculate IRR using the Newton-Raphson method
+  const calculateIRR = (cashFlowArray) => {
+    const epsilon = 0.00001; // The desired level of precision
+    let irr = 0.1; // Initial guess for IRR
+    let npv = 0;
+
+    const npvFunction = (irr, cashFlowArray) => {
+      let result = 0;
+
+      for (let i = 0; i < cashFlowArray.length; i++) {
+        result += cashFlowArray[i] / Math.pow(1 + irr, i);
+      }
+
+      return result;
+    };
+
+    const derivativeFunction = (irr, cashFlowArray) => {
+      let result = 0;
+
+      for (let i = 1; i < cashFlowArray.length; i++) {
+        result -= i * cashFlowArray[i] / Math.pow(1 + irr, i + 1);
+      }
+
+      return result;
+    };
+
+    let delta;
+    do {
+      delta = npvFunction(irr, cashFlowArray) / derivativeFunction(irr, cashFlowArray);
+      irr -= delta;
+    } while (Math.abs(delta) > epsilon);
+
+    return irr * 100; // Convert IRR to a percentage
+  };
+
+  const irrResult = calculateIRR(cashFlowArray);
+
+  // Send the IRR result to a new page
+  res.send(`The IRR is ${irrResult.toFixed(2)}%`);
+});
+
+
+
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
