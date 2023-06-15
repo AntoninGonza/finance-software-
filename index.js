0ee1423aa2e027bf9379ef7b2086c81bb7ab6099
AntoninGonza <antonin.gonzalez.crohare@gmail.com>
2023-06-15 16:46:24 +0200

    bond

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve the wacc.html file when accessing the root route
+// Serve the finance.html file when accessing the root route
 app.get('/', (req, res) => {
-  res.sendFile('wacc.html', { root: __dirname });
+  res.sendFile('finance.html', { root: __dirname });
 });
 
 // Handle form submission and calculate WACC
 app.post('/wacc', (req, res) => {
   const { costOfEquity, costOfDebt, taxRate, equityWeight, debtWeight } = req.body;
-  const wacc = (((costOfEquity/100) * equityWeight) + ((1 - (taxRate/100)) * (costOfDebt/100) * debtWeight))/10000;
+  const wacc = (((costOfEquity / 100) * equityWeight) + ((1 - (taxRate / 100)) * (costOfDebt / 100) * debtWeight)) / 10000;
 
   // Send the WACC result to a new page
   res.send(`The WACC is ${wacc}%`);
@@ -27,7 +27,7 @@ app.post('/NPV', (req, res) => {
   const NPV = -costOfProject + (RevenueYear1 / (1 + r)) + (RevenueYear2 / Math.pow(1 + r, 2)) + (RevenueYear3 / Math.pow(1 + r, 3)) + (RevenueYear4 / Math.pow(1 + r, 4));
 
   // Send the NPV result to a new page
-  res.send(`The NPV is ${NPV} $`);
+  res.send(`The NPV is $${NPV}`);
 });
 
 // Handle form submission and calculate IRR
@@ -39,7 +39,6 @@ app.post('/IRR', (req, res) => {
   const calculateIRR = (cashFlowArray) => {
     const epsilon = 0.00001; // The desired level of precision
     let irr = 0.1; // Initial guess for IRR
-    let npv = 0;
 
     const npvFunction = (irr, cashFlowArray) => {
       let result = 0;
@@ -76,8 +75,18 @@ app.post('/IRR', (req, res) => {
   res.send(`The IRR is ${irrResult.toFixed(2)}%`);
 });
 
+// Handle form submission and calculate Bond Price
+app.post('/bond', (req, res) => {
+  const { faceValue, couponRate, yearsToMaturity, discountRate } = req.body;
+  const r = discountRate / 100; // Convert discount rate to a decimal
+  const cr = couponRate / 100;
 
+  // Calculate Bond Price
+  const bondPrice = (cr * faceValue * (1 - Math.pow(1 + r, -yearsToMaturity))) / r + faceValue / Math.pow(1 + r, yearsToMaturity);
 
+  // Send the Bond Price result to a new page
+  res.send(`The Bond Price is $${bondPrice.toFixed(2)}`);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
